e8a692566b45fe6c8eb64c89e8320b4838485de1
Yasar Budulgan <yb.budulgan@gmail.com>
2020-06-14 15:16:24 +0300

    Remove app test

--- frontend/src/app/App.test.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
-
-test('renders hello world', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/Hello, World!/i);
-  expect(linkElement).toBeInTheDocument();
-});
