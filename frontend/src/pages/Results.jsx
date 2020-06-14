5585401b2ee997a483950e1588c8598f7b45ad84
Yasar Budulgan <yb.budulgan@gmail.com>
2020-06-14 15:41:38 +0300

    Update results page to show search query

--- frontend/src/pages/Results.jsx
+++ frontend/src/pages/Results.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
+import { useLocation } from '@reach/router';
+import { parse } from 'query-string';
+import useTitle from '../hooks/useTitle';
 
 export default function Resuls() {
+  const location = useLocation();
+  const searchParams = parse(location.search);
+  const query = searchParams.q;
+  const title = `${query || 'Results'} | CorpusHub`;
+  useTitle(title);
+
   return (
-    <h1>Results</h1>
+    <h1>{searchParams.q || 'Results'}</h1>
   );
 }
