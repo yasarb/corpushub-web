ad3c1ea70aef0494ec52ff7227d5f792469e8820
Yasar Budulgan <yb.budulgan@gmail.com>
2020-06-15 14:37:19 +0300

    Inject provider

--- frontend/src/app/App.jsx
+++ frontend/src/app/App.jsx
@@ -1,21 +1,22 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import { Router } from '@reach/router';
 import { ThemeProvider, CSSReset } from '@chakra-ui/core';
 
+import store from './store';
 import Home from '../pages/Home';
 import Results from '../pages/Results';
-import { QueryProvider } from '../store';
 
 function App() {
   return (
     <ThemeProvider>
       <CSSReset />
-      <QueryProvider>
+      <Provider store={store}>
         <Router>
           <Home path="/" />
           <Results path="/search" />
         </Router>
-      </QueryProvider>
+      </Provider>
     </ThemeProvider>
   );
 }
